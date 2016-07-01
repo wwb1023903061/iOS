c9d6ea229eca6b0e0e77192611d1a87b0637aac0
wang wen bing <wwb18913382275@outlook.com>
2016-07-01 10:40:20 +0800

    Revert "Revert "王文丙""
    This reverts commit 0ebc3d4bcbb63e456506c3024b70d0fa12c1ea4f.

---
+++ Runtime/Runtime/People.m
@@ -0,0 +1,19 @@
+//
+//  People.m
+//  Runtime
+//
+//  Created by yinhexin on 16/6/29.
+//  Copyright © 2016年 yinhexin. All rights reserved.
+//
+
+#import "People.h"
+
+@implementation People
+- (void)jump1 {
+    NSLog(@"jump1");
+}
+- (void)jump2 {
+    NSLog(@"jump2");
+}
+
+@end
