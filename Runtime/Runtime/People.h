0b7975c62b84325e5147406941475f2fde6cc5bd
wang wen bing <wwb18913382275@outlook.com>
2016-07-01 10:38:16 +0800

    王文丙
    this make love

---
+++ Runtime/Runtime/People.h
@@ -0,0 +1,14 @@
+//
+//  People.h
+//  Runtime
+//
+//  Created by yinhexin on 16/6/29.
+//  Copyright © 2016年 yinhexin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface People : NSObject
+- (void)jump1;
+- (void)jump2;
+@end
